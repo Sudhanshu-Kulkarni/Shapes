507dff522d59b7424e9697df2ab3f9f3e4ee3946
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-07 20:12:11 +0530

    Add methods to calculate area and perimeter and volume of an Equilateral Pyramid

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -5,5 +5,18 @@ class EquilateralPyramid extends Shape implements Volume{
         this.baseSide = baseSide;
         this.height = height;
     }
+    public double calculateArea(){
+        double baseArea = baseSide * baseSide;
+        double slantHeight = Math.sqrt((baseSide / 2) * (baseSide / 2) + height *
+                height);
+        double lateralArea = 2 * baseSide * slantHeight;
+        return baseArea + lateralArea;
+    }
 
+    public double calculatePerimeter(){
+        return 4 * baseSide;
+    }
+    public double calculateVolume(){
+        return (1.0 / 3.0) * baseSide * baseSide * height;
+    }
 }
