0bae1b9701d2b7b96f9da37cd51c00a9bc6264b3
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-07 19:08:42 +0530

    Constructor to initialise shape

---
+++ Shape.java
@@ -0,0 +1,11 @@
+//Shape.java
+
+abstract class Shape{
+	protected String shapeName;
+
+	//Zero parameters, initialising variables
+	public Shape(String shapeName){
+		this.shapeName = shapeName;
+	}
+
+}
